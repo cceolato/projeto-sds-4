fc8242defe36c2eda3058848cdc0fc42ecb101e2
Carlos A. Ceolato Jr <carlos@ceolato.com.br>
2021-09-08 18:48:14 -0300

    Domain model, database seed

---
+++ backend/src/main/java/br/com/ceolato/dsvendas/services/SaleService.java
@@ -0,0 +1,43 @@
+package br.com.ceolato.dsvendas.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ceolato.dsvendas.dto.SaleDTO;
+import br.com.ceolato.dsvendas.dto.SaleSuccessDTO;
+import br.com.ceolato.dsvendas.dto.SaleSumDTO;
+import br.com.ceolato.dsvendas.entities.Sale;
+import br.com.ceolato.dsvendas.repositories.SaleRepository;
+import br.com.ceolato.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
+}
